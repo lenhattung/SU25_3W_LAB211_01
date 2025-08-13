5dfedcba913bddc270a9872c6686bc9325003274
lenhattung <lenhattung@gmail.com>
2025-08-13 08:30:08 +0700

    update
    update

--- Project_1/src/tools/Inputter.java
+++ Project_1/src/tools/Inputter.java
@@ -19,47 +19,54 @@ public class Inputter {
         scanner = new Scanner(System.in);
     }
 
+    public String input(String message, String errorMsg, String regex) {
+        String result;
+        boolean reInput = false;
+        do {
+            System.out.println(message);
+            result = scanner.nextLine();
+            reInput = !Acceptable.isValid(result, regex);
+            if (reInput) {
+                System.out.println(errorMsg + ". Plese re-enter ... ");
+            }
+        } while (reInput);
+        return result;
+    }
+
     public Customer inputCustomer() {
         Customer customer = new Customer();
-        // input customerCode
-        String customerCode = "";
-        String customerCodeRegex = "^[CGKcgk]\\d{4}$";
-        do {
-            System.out.println("Enter Customer code\n (A unique 5-character string. The first character is \"C\", \"G\"or \"K\", followed by 4 digits.)");
-            customerCode = scanner.nextLine();
-            if (!customerCode.matches(customerCodeRegex)) {
-                System.err.println("Customer code cannot be empty! Customer code must start with C, G, K, followed by 4 digits!");
-            }
-        } while (!customerCode.matches(customerCodeRegex));
-
-        // input name
-        String name = "";
-        String nameRegex = "^.{2,25}$";
-        do {
-            System.out.println("Input name (A non-empty string between 2 and 25 characters long): ");
-            name = scanner.nextLine();
-            if (!name.matches(nameRegex)) {
-                System.err.println("Error: Name cannot be empty. Name must be between 2 and 25 characters.");
-            }
-        } while (!name.matches(nameRegex));
         
-        // input name
-        String phone = "";
-        String phoneRegex = "^.{2,25}$";
-        do {
-            System.out.println("A 10-digit number belonging to a network operator in Vietnam.");
-            name = scanner.nextLine();
-            if (!name.matches(nameRegex)) {
-                System.err.println("Error: Name cannot be empty. Name must be between 2 and 25 characters.");
-            }
-        } while (!name.matches(nameRegex));
+        // Input customerCode
+        String message = "Enter Customer code\n (A unique 5-character string. The first character is \"C\", \"G\"or \"K\", followed by 4 digits.)";
+        String errorMsg = "Customer code cannot be empty! Customer code must start with C, G, K, followed by 4 digits!";
+        String regex = Acceptable.customerCodeRegex;
+        customer.setCustomerCode(input(message, errorMsg, regex));
+
+        // Input name
+        message = "Input name (A non-empty string between 2 and 25 characters long): ";
+        errorMsg = "Error: Name cannot be empty. Name must be between 2 and 25 characters.";
+        regex = Acceptable.nameRegex;
+        customer.setName(input(message, errorMsg, regex));
+
+        // Input phone
+        message = "A 10-digit number belonging to a network operator in Vietnam.";
+        errorMsg = "Error: Invalid phone format!";
+        regex = Acceptable.phoneRegex;
+        customer.setPhoneNumber(input(message, errorMsg, regex));
+
+        // Input email
+        message = "Input email: ";
+        errorMsg = "Error: Invalid email format!";
+        regex = Acceptable.emailRegex;
+        customer.setEmail(input(message, errorMsg, regex));
 
         return customer;
     }
 
     public static void main(String[] args) {
         Inputter inputter = new Inputter();
-        inputter.inputCustomer();
+        Customer c = inputter.inputCustomer();
+        System.out.println(c);
     }
 
 }
